8cb54a348ddeb10fd997b4fb817d99361c3c1660
minorcell <minorcell6789@gmail.com>
2024-09-26 02:03:44 +0800

    style: 全局主题样式

--- src/main.jsx
+++ src/main.jsx
@@ -10,28 +10,7 @@ import zhCN from 'antd/locale/zh_CN';
 
 import './reset.css'
 import 'dayjs/locale/zh-cn';
-
-const theme = {
-    token: {
-        colorPrimary: 'rgb(107, 172, 163)',
-        fontFamily: "Koulen",
-        colorError: "rgb(107, 172, 164)",
-        borderRadiusLG: "1vh",
-    },
-    components: {
-        Table: {
-            cellFontSize: "16",
-            headerColor: "#6baca3",
-            headerSplitColor: "rgb(107, 172, 164)",
-            headerBg: "rgba(107, 172, 164, 0.2)",
-        },
-        Select: {
-            optionSelectedFontWeight: 900,
-            optionFontSize: 14,
-        }
-    }
-};
-
+import {theme} from './utils/theme.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
---
+++ src/utils/theme.js
@@ -0,0 +1,28 @@
+export const theme = {
+    token: {
+        colorPrimary: 'rgb(107, 172, 163)',
+        fontFamily: "Koulen",
+        colorError: "rgb(107, 172, 164)",
+        borderRadiusLG: "1vh",
+    },
+    components: {
+        Table: {
+            cellFontSize: "16",
+            headerColor: "#6baca3",
+            headerSplitColor: "rgb(107, 172, 164)",
+            headerBg: "rgba(107, 172, 164, 0.2)",
+        },
+        Select: {
+            optionSelectedFontWeight: 900,
+            optionFontSize: 14,
+        },
+        Descriptions: {
+            labelBg: "rgb(225, 238, 237)",
+            contentColor: "rgb(107, 172, 164)",
+            titleColor: "rgb(107, 172, 164)",
+        },
+        Image :{
+            previewOperationColor: "rgb(107, 172, 164)"
+        }
+    }
+};
