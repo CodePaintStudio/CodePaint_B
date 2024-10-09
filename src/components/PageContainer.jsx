c1dd00a375ad37cd3ff8e1a41d51e5df764c28bd
MrBean <2839878615@qq.com>
2024-10-09 13:41:32 +0800

    菜单反向高亮显示

--- src/components/PageContainer.jsx
+++ src/components/PageContainer.jsx
@@ -1,4 +1,4 @@
-import {useNavigate} from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import RouteConfig from "../router/index.jsx"
 import {
     CheckCircleFilled,
@@ -7,42 +7,42 @@ import {
     HomeFilled,
     BookFilled
 } from '@ant-design/icons';
-import {Layout, Menu, theme} from 'antd';
+import { Layout, Menu, theme } from 'antd';
 import * as style from "../styles/PageContainer.module.css"
 
-const {Sider, Content} = Layout;
+const { Sider, Content } = Layout;
 
 export default function PageContainer() {
     const navigate = useNavigate();
     const {
-        token: {colorBgContainer, borderRadiusLG},
+        token: { colorBgContainer, borderRadiusLG },
     } = theme.useToken();
 
+    const location = useLocation();
+    const selectedKey = location.pathname;
+
+    const pathToKey = {
+        '/home': '1',
+        '/articles': '2',
+        '/addarticle': '3',
+        '/handleadd': '4',
+        '/user': '5'
+    };
+
     const handleClick = (e) => {
-        switch (e.key) {
-            case '1':
-                navigate('/home');
-                break;
-            case '2':
-                navigate('/articles');
-                break;
-            case '3':
-                navigate('/addarticle');
-                break;
-            case '4':
-                navigate('/handleadd');
-                break;
-            case '5':
-                navigate('/user');
-                break;
-            default:
-                break;
-        }
+        const keyToPath = {
+            '1': '/home',
+            '2': '/articles',
+            '3': '/addarticle',
+            '4': '/handleadd',
+            '5': '/user'
+        };
+        navigate(keyToPath[e.key]);
     };
 
     return (
-        <Layout style={{maxHeight: '100vh'}}>
-            <Sider trigger={null} style={{backgroundColor: colorBgContainer}} width="13vw">
+        <Layout style={{ maxHeight: '100vh' }}>
+            <Sider trigger={null} style={{ backgroundColor: colorBgContainer }} width="13vw">
                 <div className={style.logo_box}>
                     <span
                         className={style.logo_box_text}
@@ -57,7 +57,7 @@ export default function PageContainer() {
                             }}>
                             CODEPAINT
                         </h1>
-                        <br/>
+                        <br />
                         官网后台管理系统
                     </span>
                 </div>
@@ -65,33 +65,32 @@ export default function PageContainer() {
                     horizontalItemSelectedColor="rgb(107, 172, 163)"
                     theme="light"
                     mode="inline"
-                    defaultSelectedKeys={['1']}
+                    selectedKeys={[pathToKey[selectedKey] || '']}
                     onClick={handleClick}
                     items={[
                         {
                             key: '1',
-                            icon: <HomeFilled style={{color: "rgb(107, 172, 163)"}}/>,
+                            icon: <HomeFilled style={{ color: "rgb(107, 172, 163)" }} />,
                             label: '首页',
                         },
                         {
                             key: '2',
-                            icon: <BookFilled style={{color: "rgb(107, 172, 163)"}}/>,
+                            icon: <BookFilled style={{ color: "rgb(107, 172, 163)" }} />,
                             label: '文章管理',
                         },
-
                         {
                             key: "3",
-                            icon: <EditFilled style={{color: "rgb(107, 172, 163)"}}/>,
+                            icon: <EditFilled style={{ color: "rgb(107, 172, 163)" }} />,
                             label: "新增文章"
                         },
                         {
                             key: '4',
-                            icon: <CheckCircleFilled style={{color: "rgb(107, 172, 163)"}}/>,
+                            icon: <CheckCircleFilled style={{ color: "rgb(107, 172, 163)" }} />,
                             label: '申请处理',
                         },
                         {
                             key: '5',
-                            icon: <SmileFilled style={{color: "rgb(107, 172, 163)"}}/>,
+                            icon: <SmileFilled style={{ color: "rgb(107, 172, 163)" }} />,
                             label: '个人中心',
                         }
                     ]}
@@ -105,7 +104,7 @@ export default function PageContainer() {
                     borderRadius: borderRadiusLG,
                 }}
             >
-                <RouteConfig/>
+                <RouteConfig />
             </Content>
         </Layout>
     );
